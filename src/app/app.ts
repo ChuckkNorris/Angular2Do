ffaaf824d90d844754baa3a5f2e637678475f555
Levi Fuller <levi.fuller@hotmail.com>
2015-12-09 20:53:26 -0700

    Initial Angular 2 App

---
+++ src/app/app.ts
@@ -0,0 +1,7 @@
+import {bootstrap, Component} from 'angular2/angular2';
+@Component({
+    selector: 'my-app',
+    template: '<h1>Bro, do you even angle?</h1>'
+})
+class AppComponent { }
+bootstrap(AppComponent);
