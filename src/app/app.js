65fb5cecc3bf0016d30a3b37ca0c0a28f9678381
Levi Fuller <lfuller941@gmail.com>
2015-12-09 23:12:54 -0700

    Testing TypeScript Classes & Properties

--- src/app/app.js
+++ src/app/app.js
@@ -8,13 +8,38 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require('angular2/angular2');
+var Hero = (function () {
+    function Hero(armor) {
+        // Needs to be set before Armor
+        this.armor = armor;
+        this.health = 4;
+    }
+    Object.defineProperty(Hero.prototype, "health", {
+        get: function () {
+            return this._health;
+        },
+        set: function (v) {
+            this._health = v + (3 * this.armor);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Hero;
+})();
+var Place = (function () {
+    function Place() {
+    }
+    return Place;
+})();
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = "Hero Details";
+        this.hero = new Hero(3);
     }
     AppComponent = __decorate([
         angular2_1.Component({
             selector: 'my-app',
-            template: '<h1>Bro, do you even angle?</h1>'
+            template: '<h1>{{title}}</h1><h2> {{hero.health}} {{hero.armor}} details!</h2>'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,35 @@
 import {bootstrap, Component} from 'angular2/angular2';
+class Hero {
+    constructor(armor: number) {
+        // Needs to be set before Armor
+        this.armor = armor;
+        this.health = 4;
+    }
+    id: number;
+    name: string;
+    armor: number;
+    
+    private _health : number;
+    public get health() : number {
+        return this._health;
+    }
+    public set health(v : number) {
+        this._health = v  + (3 * this.armor);
+    }
+}
+class Place {
+    name: number;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>Bro, do you even angle?</h1>'
+    template: '<h1>{{title}}</h1><h2> {{hero.health}} {{hero.armor}} details!</h2>'
 })
-class AppComponent { }
+
+
+class AppComponent {
+    public title = "Hero Details";
+   public hero = new Hero(3);
+  
+}
 bootstrap(AppComponent);
