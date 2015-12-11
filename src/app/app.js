9f52a9de80bd168c913703492aa5f73c10873092
Levi Fuller <lfuller941@gmail.com>
2015-12-10 22:20:08 -0700

    Changed to Vehicles ; List of Vehicles ; onClick event to focus selected vehicle

--- src/app/app.js
+++ src/app/app.js
@@ -8,42 +8,50 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require('angular2/angular2');
-var Hero = (function () {
-    function Hero(armor) {
-        // Needs to be set before Armor
-        this.armor = armor;
-        this.health = 4;
+// {NgModel}
+var Vehicle = (function () {
+    function Vehicle(make, name, horsePower, weightInPounds) {
+        this.make = make;
+        this.name = name;
+        this.horsePower = horsePower;
+        this.weight = weightInPounds;
     }
-    Object.defineProperty(Hero.prototype, "health", {
+    Object.defineProperty(Vehicle.prototype, "powerToWeightRatio", {
         get: function () {
-            return this._health;
-        },
-        set: function (v) {
-            this._health = v + (3 * this.armor);
+            return parseFloat((this.weight / this.horsePower).toPrecision(2));
         },
         enumerable: true,
         configurable: true
     });
-    return Hero;
-})();
-var Place = (function () {
-    function Place() {
-    }
-    return Place;
+    return Vehicle;
 })();
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = "Hero Details";
-        this.hero = new Hero(3);
+        this.title = "Vehicle Details";
+        this.selectedVehicle = new Vehicle("BMW", "E30 M3", 192, 2700);
+        this.vehicles = VEHICLES;
     }
+    AppComponent.prototype.onSelect = function (vehicle) { this.selectedVehicle = vehicle; };
     AppComponent = __decorate([
         angular2_1.Component({
             selector: 'my-app',
-            template: '<h1>{{title}}</h1><h2> {{hero.health}} {{hero.armor}} details!</h2>'
+            directives: [angular2_1.FORM_DIRECTIVES, angular2_1.NgFor],
+            styles: ["\n        .vehicles {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}\n        .vehicles li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }\n        .vehicles li:hover {color: #369; background-color: #EEE; left: .2em;}\n        .vehicles .badge {\n            font-size: small;\n            color: white;\n            padding: 0.1em 0.7em;\n            background-color: #369;\n            line-height: 1em;\n            position: relative;\n            left: -1px;\n            top: -1px;\n        }\n        .selected { background-color: #EEE; color: #369; }\n"],
+            // MultiLine templates by using tilde key
+            // {{OneWayBinding}}
+            // [(ng-model)]="TwoWayDataBinding"
+            template: "\n    <h2>My Garage</h2>\n    <ul class=\"vehicles\">\n        <!--* means Element and Children constitute master template-->\n        <!--#vehicle indicates local template variable-->\n        <li *ng-for=\"#vehicle of vehicles\" (click)=\"onSelect(vehicle)\">\n            <span class=\"badge\">{{vehicle.make}}</span> {{vehicle.name}}\n        </li>\n    </ul>\n    \n    <h1>{{title}}</h1>\n    <h2>{{selectedVehicle.name}}</h2>\n    <label>Name</label>\n        <div>\n            <input [(ng-model)]=\"selectedVehicle.name\" placeholder=\"name\">\n            {{selectedVehicle.name}}\n        </div>\n    <h2> {{selectedVehicle.horsePower}} hp, {{selectedVehicle.weight}} lbs, {{selectedVehicle.powerToWeightRatio}} lbs/hp</h2>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 })();
+var VEHICLES = [
+    new Vehicle("BMW", "E30 M3", 192, 2700),
+    new Vehicle("Yamaha", "FZ-09", 110, 414),
+    new Vehicle("Porsche", "911 Carrera S", 430, 3075),
+    new Vehicle("Nissan", "GT-R", 545, 3813),
+    new Vehicle("Ferrari", "458 Italia", 597, 3076)
+];
 angular2_1.bootstrap(AppComponent);
 //# sourceMappingURL=app.js.map
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,35 +1,81 @@
-import {bootstrap, Component} from 'angular2/angular2';
-class Hero {
-    constructor(armor: number) {
-        // Needs to be set before Armor
-        this.armor = armor;
-        this.health = 4;
+import {bootstrap, Component, FORM_DIRECTIVES, NgFor} from 'angular2/angular2';
+// {NgModel}
+class Vehicle {
+    constructor(make: string, name: string, horsePower: number, weightInPounds: number) {
+        this.make = make;
+        this.name = name;
+        this.horsePower = horsePower;
+        this.weight = weightInPounds;
     }
-    id: number;
     name: string;
-    armor: number;
+    make: string;
+    horsePower: number;
+    weight: number;
     
-    private _health : number;
-    public get health() : number {
-        return this._health;
+    public get powerToWeightRatio() : number {
+        return parseFloat((this.weight / this.horsePower).toPrecision(2));
     }
-    public set health(v : number) {
-        this._health = v  + (3 * this.armor);
-    }
-}
-class Place {
-    name: number;
+    
+        
 }
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2> {{hero.health}} {{hero.armor}} details!</h2>'
+    directives: [FORM_DIRECTIVES, NgFor],
+    styles:[`
+        .vehicles {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+        .vehicles li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+        .vehicles li:hover {color: #369; background-color: #EEE; left: .2em;}
+        .vehicles .badge {
+            font-size: small;
+            color: white;
+            padding: 0.1em 0.7em;
+            background-color: #369;
+            line-height: 1em;
+            position: relative;
+            left: -1px;
+            top: -1px;
+        }
+        .selected { background-color: #EEE; color: #369; }
+`],
+
+    // MultiLine templates by using tilde key
+    // {{OneWayBinding}}
+    // [(ng-model)]="TwoWayDataBinding"
+    template: `
+    <h2>My Garage</h2>
+    <ul class="vehicles">
+        <!--* means Element and Children constitute master template-->
+        <!--#vehicle indicates local template variable-->
+        <li *ng-for="#vehicle of vehicles" (click)="onSelect(vehicle)">
+            <span class="badge">{{vehicle.make}}</span> {{vehicle.name}}
+        </li>
+    </ul>
+    
+    <h1>{{title}}</h1>
+    <h2>{{selectedVehicle.name}}</h2>
+    <label>Name</label>
+        <div>
+            <input [(ng-model)]="selectedVehicle.name" placeholder="name">
+            {{selectedVehicle.name}}
+        </div>
+    <h2> {{selectedVehicle.horsePower}} hp, {{selectedVehicle.weight}} lbs, {{selectedVehicle.powerToWeightRatio}} lbs/hp</h2>
+    `
 })
 
 
 class AppComponent {
-    public title = "Hero Details";
-   public hero = new Hero(3);
+    public title = "Vehicle Details";
+    public selectedVehicle = new Vehicle("BMW", "E30 M3", 192, 2700);
+    public vehicles = VEHICLES;
+    onSelect(vehicle: Vehicle) { this.selectedVehicle = vehicle}
   
 }
+var VEHICLES: Vehicle[] = [
+    new Vehicle("BMW", "E30 M3", 192, 2700),
+    new Vehicle("Yamaha", "FZ-09", 110, 414),
+    new Vehicle("Porsche", "911 Carrera S", 430, 3075),
+    new Vehicle("Nissan", "GT-R", 545, 3813),
+    new Vehicle("Ferrari", "458 Italia", 597, 3076)
+]
 bootstrap(AppComponent);
